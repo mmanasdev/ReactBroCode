5e8ab6283059d58fa9109f8db486cbb5b7bef991
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2024-09-27 12:32:38 +0200

    css styles

---
+++ my-react-project/src/StylesLesson.jsx
@@ -0,0 +1,19 @@
+/*
+How to style react components with CSS
+(not including external frameworks or preprocessors)
+
+1. external
+2. modules
+3. inline
+*/
+import Button from "./Button/Button"
+
+function StylesLesson() {
+
+  return <div>
+    <Button />
+  </div>
+
+}
+
+export default StylesLesson
--- my-react-project/src/main.jsx
+++ my-react-project/src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 // import Basics from './Basics.jsx'// 1. BasicsLesson
-import CardLesson from './CardLesson.jsx'// 2. Card component Lesson
+// import CardLesson from './CardLesson.jsx'// 2. Card component Lesson
+import StylesLesson from './StylesLesson'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CardLesson />
+    <StylesLesson />
   </StrictMode>,
 )
